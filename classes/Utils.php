0bb86e3a30fc3c9729f7c97795f9142a8de32ffe
Micah Andrew Bule <micah.bule@staizen.com>
2023-03-07 13:57:47 +0800

    feat: remove sources and ewallet classes

--- classes/Utils.php
+++ classes/Utils.php
@@ -71,8 +71,4 @@ class Utils {
             ),
         ));
     }
-
-    public function getSourceReturnUrl($status, $order_id, $plugin_version) {
-        return get_home_url() . '/?wc-api=cynder_paymongo_catch_source_redirect&order=' . $order_id . '&status=' . $status . '&agent=cynder_woocommerce&version=' . $plugin_version;
-    }
 }
